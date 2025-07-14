b7ffa0aba67e11724d8bc40dcdfab4b355351f51
DJ-no1 <darkjargon739@gmail.com>
2025-07-14 16:01:00 +0530

    basic flow todo

---
+++ todo/main.py
@@ -0,0 +1,59 @@
+from langchain_google_genai import GoogleGenerativeAI
+import dotenv
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Literal
+dotenv.load_dotenv()
+
+savedList = {'savedList': 'Here is your task list:\n\n*   Buy groceries\n*   Call mom\n*   Finish project report on operating system\n*   Finish laundry\n*   Call manager about meeting', 'category': "Here's the categorization of your tasks:\n\n**Work:**\n*   Call manager about meeting\n\n**College:**\n*   Finish project report on operating system\n\n**Personal:**\n*   Buy groceries\n*   Call mom\n*   Finish laundry"}
+
+# model initialization
+model = GoogleGenerativeAI(
+    model = "gemini-2.5-flash"
+    )
+
+
+
+class todoState(TypedDict):
+    userInput: str
+    taskList: list[str]
+    category: Literal["work", "college", "personal"] | None
+    savedList: dict[str, str]
+
+def task_List(state: todoState) -> todoState:
+    userInput = state["userInput"]
+    prompt = f"Create a task list based on the following conversation: {userInput} , make sure the tasks from the conversation only , do not add any extra tasks. "
+    taskList = model.invoke(prompt)
+    state["taskList"] = taskList
+    return state
+
+def categoryList(state: todoState) -> todoState:
+    taskList = state["taskList"]
+    prompt = f"Categorize the following tasks into work, college, and personal categories: {taskList} , make sure to categorize each task only once. "
+    category = model.invoke(prompt)
+    state["category"] = category
+    return state
+
+
+# defining graph
+graph = StateGraph(todoState)
+
+#adding nodes
+graph.add_node('taskList', task_List)
+graph.add_node('categoryList', categoryList)
+
+#adding edges
+graph.add_edge(START, 'taskList')
+graph.add_edge('taskList', 'categoryList')
+graph.add_edge('categoryList', END)
+
+# compile graph
+workflow = graph.compile()
+
+
+# Execute the graph
+
+initial_state = {'userInput': "“I need to buy groceries, call mom, and finish my project report on operating system by tomorrow. I also need to finish my laundry later today. and call my manager about the meeting tomorrow."}
+
+final_state = workflow.invoke(initial_state)
+
+print(final_state)
